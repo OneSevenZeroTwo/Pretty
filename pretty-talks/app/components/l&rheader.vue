73201abb66680534a1bb919bf92c0d2ce3bf215f
manzerpan <manzerpan@foxmail.com>
2017-07-25 21:03:22 +0800

    login

---
+++ pretty-talks/app/components/l&rheader.vue
@@ -0,0 +1,46 @@
+<template>
+	<header>
+		<div>
+			<img src="../../images/login&reg&mine/back.png" />
+		</div>
+		<div class="mid">
+			<a href="">{{theHeader.title}}</a>
+		</div>
+		<div>
+			<a href="">{{theHeader.rightBtn}}</a>
+		</div>
+	</header>
+</template>
+<style>
+	header {
+		height: 45px;
+		width: 100%;
+		overflow: hidden;
+		text-align: center;
+		line-height: 45px;
+		border-bottom: 1px solid rgba(51, 51, 51, .15);
+	}
+	
+	header div {
+		float: left;
+		width: 15%;
+	}
+	
+	header img {
+		height: 14px;
+	}
+	
+	header a {
+		color: #666;
+		font-size: 18px;
+	}
+	
+	.mid {
+		width: 70%;
+	}
+</style>
+<script>
+	export default {
+		props:['theHeader'],
+	}
+</script>
--- pretty-talks/app/main.js
+++ pretty-talks/app/main.js
@@ -3,23 +3,27 @@ import Vuex from "vuex";
 import VueRouter from "vue-router";
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 import axios from "axios";
+import MuseUI from 'muse-ui';
+import 'muse-ui/dist/muse-ui.css';
 
+Vue.use(MuseUI);
 Vue.use(Vuex);
 Vue.use(VueRouter);
 Vue.use(VueAwesomeSwiper);
 Vue.prototype.$ajax = axios;
 
+import "./css/base.css";
 import Plogin from "./router/plogin.vue";
 
 var store = new Vuex.Store({
 	state: {
-		
-	},
-	mutations: {
 
 	},
-	actions: {
+	mutations: {
 		
+	},
+	actions: {
+
 	}
 })
 var router = new VueRouter({
