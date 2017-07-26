80462e7b8acf2c9aa52972795c995d1ddd76429f
jhao520 <371182070@qq.com>
2017-07-26 21:23:06 +0800

    car..30%

--- pretty-talks/app/main.js
+++ pretty-talks/app/main.js
@@ -58,18 +58,44 @@ var store = new Vuex.Store({
 	state: {
 		carList: [],
 		pid: null,
+		carProId: null,
+		carProNum:null,
+		delList:true,
 	},
 	getters: {
 
 	},
 	//分发状态
 	mutations: {
+		//获取购物车列表数据
 		setCarList(state) {
-			axios.get("http://localhost:8080/carlist.json")
+			axios.get("http://localhost:5555/read")
 				.then((res) => {
-					//console.log(res);
-					state.carList = res.data.data;
+					state.carList = state.carList.concat(res.data);
 					console.log(state.carList);
+				}).catch((err) => {})
+		},
+		//修改购物车列表选中项数据
+		getCarList(state) {
+			axios.get("http://localhost:5555/write", {
+					params: {
+						id: state.carProId,
+						num:state.carProNum,
+					}
+				})
+				.then((res) => {
+					console.log("数据写入成功："+res);
+				}).catch((err) => {})
+		},
+		//删除购物车列表选中项数据
+		delCarList(state) {
+			axios.get("http://localhost:5555/delete", {
+					params: {
+						id: state.carProId,
+					}
+				})
+				.then((res) => {
+					console.log("数据删除成功："+res);
 				}).catch((err) => {})
 		},
 		setNews(state) {
@@ -79,7 +105,7 @@ var store = new Vuex.Store({
 					},
 				})
 				.then((response) => {
-					//					state.news = state.news.concat(response.data.data)
+					//state.news = state.news.concat(response.data.data)
 				})
 				.catch((error) => {
 					console.log(error);
@@ -93,7 +119,7 @@ var store = new Vuex.Store({
 					}
 				}).then((response) => {
 					console.log(response)
-					//					state.res = response.data.data
+					//state.res = response.data.data
 					console.log(state.res)
 				})
 				.catch((error) => {
@@ -104,8 +130,17 @@ var store = new Vuex.Store({
 	},
 
 	actions: {
+		//提交触发 mutations 的 setCarList 获取数据函数
 		setCarList(context) {
 			context.commit("setCarList");
+		},
+		//提交触发 mutations 的 getCarList 修改数据函数
+		getCarList(context) {
+			context.commit("getCarList");
+		},
+		//提交触发 mutations 的 getCarList 修改数据函数
+		delCarList(context) {
+			context.commit("delCarList");
 		},
 		setNews(context, data) {
 			context.commit('setNews')
@@ -118,33 +153,31 @@ var store = new Vuex.Store({
 
 var router = new VueRouter({
 	routes: [{
-			path: '/index',
-			component: Pindex,
-			children: [{
-				path: 'home',
-				component: Phome,
-			}, {
-				path: '/category',
-				component: Psort
-			}]
-		},
-		{
-			path: '/subCategory',
-			component: sub,
+		path: '/index',
+		component: Pindex,
+		children: [{
+			path: 'home',
+			component: Phome,
 		}, {
-			path: '/login',
-			component: Plogin
-		}, {
-			path: "/car",
-			component: car
-		}, {
-			path: '/reg',
-			component: Preg
-		}, {
-			path: '/',
-			redirect: 'index/home'
-		}
-	]
+			path: '/category',
+			component: Psort
+		}]
+	}, {
+		path: '/subCategory',
+		component: sub,
+	}, {
+		path: '/login',
+		component: Plogin
+	}, {
+		path: "/car",
+		component: car
+	}, {
+		path: '/reg',
+		component: Preg
+	}, {
+		path: '/',
+		redirect: 'index/home'
+	}]
 
 })
 
